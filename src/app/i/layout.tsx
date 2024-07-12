2bd9a89977630f359d1f62c739c978623f6efed3
MaRage <marat_gadzhiev_2003@mail.ru>
2024-07-12 19:27:46 +0300

    Create header, sidebar and settings page

--- src/app/i/layout.tsx
+++ src/app/i/layout.tsx
@@ -1,5 +1,7 @@
 import type { PropsWithChildren } from 'react';
 
+import DashboardLayout from '@/components/dashboard-layout/DashboardLayout';
+
 export default function Layout({ children }: PropsWithChildren<unknown>) {
-  return <div>{children}</div>;
+  return <DashboardLayout>{children}</DashboardLayout>;
 }
---
+++ src/hooks/useUpdateSettings.ts
@@ -0,0 +1,25 @@
+import {
+  QueryClient,
+  useMutation,
+  useQueryClient,
+} from '@tanstack/react-query';
+import { toast } from 'sonner';
+
+import type { TypeUserForm } from '@/types/auth.types';
+
+import { userService } from '@/services/user.service';
+
+export function useUpdateSettings() {
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending } = useMutation({
+    mutationKey: ['update profile'],
+    mutationFn: (data: TypeUserForm) => userService.update(data),
+    onSuccess() {
+      toast.success('Successfully update profile!');
+      queryClient.invalidateQueries({ queryKey: ['profile'] });
+    },
+  });
+
+  return { mutate, isPending };
+}
